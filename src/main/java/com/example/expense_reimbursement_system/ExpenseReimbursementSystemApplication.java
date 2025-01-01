b595080e20b6bc7a86ebe00579668585e0e57366
samiullah <samikhandev2k@gmail.com>
2025-01-01 16:28:28 +0500

    chore: deployed my project on docker

--- src/main/java/com/example/expense_reimbursement_system/ExpenseReimbursementSystemApplication.java
+++ src/main/java/com/example/expense_reimbursement_system/ExpenseReimbursementSystemApplication.java
@@ -1,8 +1,11 @@
 package com.example.expense_reimbursement_system;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "Expense Reimbursement API", version = "1.0"))
 @SpringBootApplication
 public class ExpenseReimbursementSystemApplication {
 
